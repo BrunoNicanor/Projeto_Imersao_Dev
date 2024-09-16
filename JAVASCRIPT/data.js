8130c59ea4e06db02729e954c716f6dd985b866b
Bruno Nicanor <bruno.hero01@gmail.com>
2024-09-16 16:16:16 -0300

    Adicionando imagens

--- JAVASCRIPT/data.js
+++ JAVASCRIPT/data.js
@@ -10,31 +10,37 @@ const personagens = [
     {
         nome: "homem aranha",
         descricao: "O Homem-Aranha, cuja identidade secreta é Peter Parker, é um herói da Marvel criado por Stan Lee e Steve Ditko. Ele ganhou seus poderes após ser mordido por uma aranha radioativa, o que lhe deu força, agilidade, sentido-aranha e a habilidade de escalar paredes. Após a morte de seu tio Ben, Peter adota o lema 'Com grandes poderes, vêm grandes responsabilidades'. Sua primeira aparição foi em *Amazing Fantasy* #15, em 1962.",
-        link: "https://pt.wikipedia.org/wiki/Homem-Aranha"
+        link: "https://pt.wikipedia.org/wiki/Homem-Aranha",
+        imagem: "/IMAGE/Personagens/homeAranha.png"
     },
     {
         nome: "homem de ferro",
-        descricao: "Tony Stark, o Homem de Ferro, é um bilionário, engenheiro e filantropo que cria uma armadura de alta tecnologia para proteger a si mesmo e ao mundo. Sua inteligência e recursos financeiros fazem dele um dos maiores heróis da Marvel. Ele é conhecido por seu sarcasmo e seu estilo de vida extravagante.",
-        link: "https://pt.wikipedia.org/wiki/Homem_de_Ferro"
+        descricao: "O Homem de Ferro, alter ego de Tony Stark, é um dos principais heróis da Marvel. Criado por Stan Lee, Larry Lieber, Don Heck e Jack Kirby, ele fez sua primeira aparição em Tales of Suspense #39, em 1963. Tony é um gênio bilionário e filantropo que, após ser sequestrado e ferido, desenvolve uma armadura tecnológica para salvar sua vida e escapar. Ele aprimora a armadura para combater o crime e ameaças globais como o Homem de Ferro. A armadura concede a Tony habilidades como superforça, voo, resistência a ataques e um vasto arsenal de armas avançadas. Tony também é conhecido por seu brilhantismo tecnológico e por ser um membro fundador dos Vingadores, além de uma figura central no Universo Cinematográfico da Marvel (MCU).",
+        link: "https://pt.wikipedia.org/wiki/Homem_de_Ferro",
+        imagem: "/IMAGE/Personagens/homemDeFerro.png"
     },
     {
         nome: "capitã marvel",
-        descricao: "Carol Danvers, a Capitã Marvel, é uma das heroínas mais poderosas da Marvel. Com seus poderes cósmicos, ela possui força sobre-humana, a capacidade de voar e manipular energia. A Capitã Marvel é conhecida por sua coragem e determinação.",
-        link: "https://pt.wikipedia.org/wiki/Capitã_Marvel_(Marvel_Comics)"
+        descricao: "Carol Danvers, a Capitã Marvel, é uma heroína poderosa da Marvel criada por Roy Thomas e Gene Colan. Sua primeira aparição foi em Marvel Super-Heroes #13, em 1968. Ex-piloto da Força Aérea, Carol ganhou habilidades super-humanas após ser exposta a uma explosão de tecnologia alienígena Kree. Ela possui força, durabilidade e velocidade aumentadas, além de ser capaz de voar e disparar rajadas de energia. A Capitã Marvel também desempenha um papel importante em aventuras cósmicas, defendendo a Terra e o universo.",
+        link: "https://pt.wikipedia.org/wiki/Captain_Marvel_(Marvel_Comics)",
+        imagem: "/IMAGE/Personagens/CapitaMarvel.png"
     },
     {
         nome: "thor",
-        descricao: "Thor é um deus nórdico do trovão e um dos Vingadores mais poderosos. Ele possui força sobre-humana, a capacidade de voar e controlar o clima. Thor é conhecido por seu martelo Mjolnir, que apenas aqueles dignos podem empunhar.",
-        link: "https://pt.wikipedia.org/wiki/Thor_(Marvel_Comics)"
+        descricao: "Thor, o Deus do Trovão, é um dos heróis mais poderosos da Marvel, baseado na mitologia nórdica. Criado por Stan Lee, Larry Lieber e Jack Kirby, Thor fez sua estreia em Journey into Mystery #83, em 1962. Ele é o príncipe de Asgard, um reino mítico, e filho de Odin, o governante de Asgard. Thor empunha o martelo mágico Mjolnir, que só pode ser levantado por aqueles que são dignos, e isso lhe dá controle sobre o trovão, além de permitir que ele voe e ataque com poderosas rajadas de energia. Como um deus, ele também tem superforça, invulnerabilidade e longevidade. Thor é um dos Vingadores originais e atua como protetor tanto da Terra quanto de Asgard, enfrentando inimigos místicos e cósmicos.",
+        link: "https://pt.wikipedia.org/wiki/Thor",
+        imagem: "/IMAGE/Personagens/thor.png"
     },
     {
         nome: "hulk",
-        descricao: "Bruce Banner, o Hulk, é um cientista que se transforma em um monstro verde e incontrolável quando fica com raiva. O Hulk possui força sobre-humana e uma resistência quase invulnerável. A luta de Banner para controlar sua alter ego é um dos temas centrais da história do personagem.",
-        link: "https://pt.wikipedia.org/wiki/Hulk"
+        descricao: "O Hulk, cujo nome verdadeiro é Dr. Bruce Banner, foi criado por Stan Lee e Jack Kirby, aparecendo pela primeira vez em The Incredible Hulk #1, em 1962. Bruce Banner, um brilhante cientista, foi exposto à radiação gama durante um experimento que deu errado, o que o transformou no Hulk, uma entidade de força imensa e quase indestrutível. Sempre que Banner se enfurece, ele se transforma no Hulk, que possui força, resistência e invulnerabilidade sobre-humanas, além de uma capacidade de cura extraordinária. Embora o Hulk seja uma força de destruição incontrolável, ele frequentemente é um herói quando seus poderes são canalizados para o bem, sendo também membro dos Vingadores.",
+        link: "https://pt.wikipedia.org/wiki/Hulk",
+        imagem: "/IMAGE/Personagens/hulk.png"
     },
     {
         nome: "viúva negra",
-        descricao: "Natasha Romanoff, a Viúva Negra, é uma ex-agente da KGB que se tornou uma das maiores espiãs e agentes da S.H.I.E.L.D. Ela é uma mestre em artes marciais, combate corpo a corpo e espionagem. A Viúva Negra é conhecida por sua inteligência, agilidade e lealdade aos seus amigos.",
-        link: "https://pt.wikipedia.org/wiki/Viúva_Negra_(personagem)"
+        descricao: "A Viúva Negra, alter ego de Natasha Romanoff, é uma espiã e heroína habilidosa. Criada por Stan Lee, Don Rico e Don Heck, apareceu pela primeira vez em Tales of Suspense #52, em 1964. Natasha foi treinada pelo governo soviético como uma espiã e assassina, mas eventualmente desertou para os Estados Unidos e se tornou membro dos Vingadores. Embora não tenha superpoderes, Natasha é uma combatente experiente, mestre em artes marciais e uso de armas, e uma espiã altamente treinada, reconhecida por sua inteligência e habilidades furtivas.",
+        link: "https://pt.wikipedia.org/wiki/Viúva_Negra_(Marvel_Comics)",
+        imagem: "/IMAGE/Personagens/viuvaNegra.png"
     }
 ];
--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,7 +4,7 @@ function pesquisar(){
 for(let dado of personagens){
     section.innerHTML += `
     <section class="Conteudo">
-        <img src="/IMAGE/Personagens/homeAranha.png" alt="">
+        <img src="${dado.imagem}" alt="">
         <div class="Conteudo_texto">
             <h3>${dado.nome}</h3>
             <p>${dado.descricao}</p>
