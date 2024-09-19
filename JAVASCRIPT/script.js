30578f0e70f71c959e99acea080b6766a345037c
Sidnei Nicanor da Silva <bruno.hero01@gmail.com>
2024-09-19 18:02:54 -0300

    Adicionando condicionais para caso o usuário deixar o input vazio ou pesquisar algum personagem que não existe

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,13 +1,26 @@
-console.log(personagens);
 function pesquisar(){
     let section = document.getElementById("dados_pesquisa");
 
     let conteudoPesquisa = document.getElementById("conteudo-pesquisa").value;
     conteudoPesquisa = conteudoPesquisa.toLowerCase();
 
+    let personagemEncontrado = false;
+
+    if(conteudoPesquisa == ""){
+        section.innerHTML = `
+        <section class="Conteudo_Aviso">
+            <div class="Conteudo_texto">
+                <h3>Nenhum Personagem encontrado</h3>
+            </div>
+        </section>
+        `
+        return
+    }
+
     for(let dado of personagens){
 
         if(dado.nome.includes(conteudoPesquisa)){
+            personagemEncontrado = true;    
             section.innerHTML = `
             <section class="Conteudo">
                 <img src="${dado.imagem}" alt="">
@@ -15,6 +28,16 @@ function pesquisar(){
                     <h3>${dado.nome}</h3>
                     <p>${dado.descricao}</p>
                     <a class="leia_mais" target="_blank" href="${dado.link}">Leia mais</a>
+                </div>
+            </section>
+            `
+        }
+
+        if(!personagemEncontrado){
+            section.innerHTML = `
+            <section class="Conteudo_Aviso">
+                <div class="Conteudo_texto">
+                    <h3>Nenhum Personagem encontrado</h3>
                 </div>
             </section>
             `
